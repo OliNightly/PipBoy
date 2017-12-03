58d2b70d21b556ed8013c545417e3f15aa8a303c
OliNightly <oli@nightly.pw>
2017-12-03 13:17:02 +0000

    XCODEProject
    I've added the xcodeproject!

---
+++ xcodeproject/Pip-Boy/Pip-Boy/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  web load
+//
+//  Created by Sebastian Hette on 11.12.2016.
+//  Copyright © 2016 MAGNUMIUM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myWebView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if myWebView.boolValue {
+            myWebView.mediaPlaybackRequiresUserAction = NO;
+        }
+        
+        let url = URL(string: "http://nightly.pw/pipboy/ios/")
+        myWebView.loadRequest(URLRequest(url: url!))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+
+
+
